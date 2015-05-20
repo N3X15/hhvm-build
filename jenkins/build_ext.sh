e63c9813248484e1b4e8f2447c39fadb7afe0072
Rob Nelson <nexisentertainment@gmail.com>
2015-05-19 20:32:24 -0700

    Separate Jenkins and Local configs.

--- jenkins/build_ext.sh
+++ jenkins/build_ext.sh
@@ -1,1 +1,1 @@
-./hhvm_build_ext.py deb jessie $1 --force-rebuild
+./hhvm_build_ext.py deb jessie $1 --force-rebuild -c jenkins.yml
--- jenkins/nightly.sh
+++ jenkins/nightly.sh
@@ -1,1 +1,1 @@
-./hhvm_build.py deb jessie 3.7.0-nightly ./hhvm_src --force-rebuild
+./hhvm_build.py deb jessie 3.7.0-nightly ./hhvm_src --force-rebuild -c jenkins.yml
